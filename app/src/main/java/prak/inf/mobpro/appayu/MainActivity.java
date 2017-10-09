7acc0f5ca4b80e0e480cf57c00f1bc42ee06a3fb
iu0211 <ayuisnaini0211@gmail.com>
2017-10-09 16:02:34 +0700

    Nambahin code program biar cucokk

--- app/src/main/java/prak/inf/mobpro/appayu/MainActivity.java
+++ app/src/main/java/prak/inf/mobpro/appayu/MainActivity.java
@@ -2,12 +2,16 @@ package prak.inf.mobpro.appayu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView teks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teks = (TextView) findViewById(R.id.teks);
     }
 }
